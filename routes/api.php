c2af0a5546d3c8c4ed94721a0fed93d6cf455d92
Christian Chandra <chanchrisid@gmail.com>
2025-06-18 16:43:34 +0700

    feat: add logout endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+
+
 Route::post('/register', RegisterController::class);
 Route::post('/login', LoginController::class);
+
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
