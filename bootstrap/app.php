6e4b435ab50ed6e211bc8c4ac435c5d64205e782
Christian Chandra <chanchrisid@gmail.com>
2025-06-19 17:36:02 +0700

    feat: add custom handler for find or fail

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use Spatie\Permission\Exceptions\UnauthorizedException;
 use Illuminate\Http\JsonResponse;
 use App\Http\Resources\ErrorResource;
 use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -35,5 +36,11 @@ return Application::configure(basePath: dirname(__DIR__))
                 ->response()
                 ->setStatusCode(401);
             // return response()->json(['message' => 'Unauthenticated'], 401);
+        });
+        $exceptions->render(function (NotFoundHttpException $exception): JsonResponse {
+            return (new ErrorResource(['message' => 'Resource not found']))
+                ->response()
+                ->setStatusCode(404);
+            // return response()->json(['message' => 'Resource not found'], 404);
         });
     })->create();
