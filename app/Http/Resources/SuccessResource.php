4fd6d6e712b440d280d76b17997752caf923b433
Christian Chandra <chanchrisid@gmail.com>
2025-06-19 17:35:45 +0700

    feat: add success resource

---
+++ app/Http/Resources/SuccessResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SuccessResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $res = [];
+
+        if(isset($this->data)) $res['error'] = $this->data;
+        return $res;
+    }
+
+    public function withResponse(Request $request, $response): void
+    {
+        $response->setData(array_merge([
+            'status' => false,
+            'message' => $this->resource['message'] ?? 'An error occurred',
+        ], $response->getData(true)));
+    }
+}
